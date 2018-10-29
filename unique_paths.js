e05e6b6c876de8e2002a2b0fd9b9d8a1c7e2db9f
Daniel <d.chen323@gmail.com>
2018-10-29 12:30:30 -0700

    Complete unique paths problem

---
+++ unique_paths.js
@@ -0,0 +1,46 @@
+/*
+  A robot is located at the top-left corner of a m x n grid.
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+
+Above is a 7 x 3 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+*/
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+  const grid = [];
+  for (let i = 0; i < n; i++) {
+    grid.push(new Array(m).fill(1));
+  }
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < m; j++) {
+      grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+    }
+  }
+
+  return grid[n - 1][m - 1];
+};
