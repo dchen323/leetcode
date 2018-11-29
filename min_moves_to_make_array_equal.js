4b6d2d1747fd34c9b2c314046ff5e39046900470
Daniel <d.chen323@gmail.com>
2018-11-29 11:40:21 -0800

    Complete min moves to make array equal problem

---
+++ min_moves_to_make_array_equal.js
@@ -0,0 +1,37 @@
+/*
+Given a non-empty integer array, find the minimum number of moves required to make all array elements equal, where a move is incrementing a selected element by 1 or decrementing a selected element by 1.
+
+You may assume the array's length is at most 10,000.
+
+Example:
+
+Input:
+[1,2,3]
+
+Output:
+2
+
+Explanation:
+Only two moves are needed (remember each move increments or decrements one element):
+
+[1,2,3]  =>  [2,2,3]  =>  [2,2,2]
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves2 = function(nums) {
+  let count = 0;
+  if (!nums || !nums.length) return count;
+
+  nums = nums.sort((a, b) => a - b);
+
+  const middle = nums[Math.floor(nums.length / 2)];
+
+  for (let i = 0; i < nums.length; i++) {
+    count += Math.abs(middle - nums[i]);
+  }
+
+  return count;
+};
